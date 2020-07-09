538b0430196ee9838bea14b9ad9b7a87e1f7f94d
ihzi999 <68045809+ihzi999@users.noreply.github.com>
2020-07-09 17:16:32 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    nb += 1
-    basic.showString("" + (nb))
-    if (nb == 10) {
-        nb = 0
-    }
+	
 })
-let nb = 0
-nb = 1
+let nb = 1
 basic.showString("" + (nb))
 basic.forever(function () {
 	
